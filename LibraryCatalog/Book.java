4793c0cbd5a78d37fdef2caa88dfc93cadfe8cf9
Furaku-p <alfianzlovenkary@gmail.com>
2025-12-01 13:58:29 +0700

    Add files via upload

---
+++ LibraryCatalog/Book.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class Book here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+    
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+    
+    public String getAuthor() {
+        return author;
+    }
+    
+    public int getYear() {
+        return year;
+    }
+    
+    @Override
+    public String toString(){
+        return "Judul: " + title + " | Penulis: " + author + " | Tahun: " + year;
+    }
+}
